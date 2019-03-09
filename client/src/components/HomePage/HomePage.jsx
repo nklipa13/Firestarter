130ca5ceefe27bbf45f928e57edab58071952ac7
Cussone <aleksandar.cakalic@gmail.com>
2019-03-09 14:44:58 +0100

    Home page.

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './HomePage.scss';
 
 const HomePage = () => (
-  <div>
-    Firestarter
+  <div className="home width-container flex">
+    <h1 className="heading-2 text-large-margin">
+      Support projects on Ethereum
+      <br />
+      and watch them grow
+    </h1>
+    <h2 className="heading-4 text-large-margin">
+      Firestarter introduces various projects built on Ethereum and helps
+      developers
+      <br />
+      getting their projects funded by community.
+    </h2>
+
+    <div className="projects">
+      <Link to="/project/0" className="project">
+        <h3>Community-driven Dapps on Ethereum Blockchain</h3>
+        <img src="https://bit.ly/2SUf41r" alt="Community-driven Dapps on Ethereum Blockchain"/>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          nulla ante, pretium vel neque. Praesent nulla ante, pretium vel neque.
+          Praesent nulla asda.
+        </h4>
+      </Link>
+
+      <Link to="/project/0" className="project">
+        <h3>Community-driven Dapps on Ethereum Blockchain</h3>
+        <img src="https://bit.ly/2SUf41r" alt="Community-driven Dapps on Ethereum Blockchain"/>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          nulla ante, pretium vel neque. Praesent nulla ante, pretium vel neque.
+          Praesent nulla asda.
+        </h4>
+      </Link>
+    </div>
+
+    <div className="cta">
+      <h2 className="heading-3 text-large-margin">Are you building a community-driven dapp?</h2>
+      <h3 className="heading-4 text-large-margin">Start your project with Firestrater and fund your project easily.</h3>
+      <Link to="/start-project" className="button  uppercase no-wrap">Support the project</Link>
+    </div>
+
   </div>
 );
 
