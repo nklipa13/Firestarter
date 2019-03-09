f9bf5e93b09e0d904a9571b2071036c30e9ecad3
Nenad Palinkasevic <nesa993@gmail.com>
2019-03-09 17:01:02 +0100

    Get project id

--- server/test.js
+++ server/test.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 const Web3 = require('web3');
 
 // var ENS = require('ethereum-ens');
-var ens = require('ethjs-ens');
+// var ens = require('ethjs-ens');
 
-const web3 = new Web3(new Web3.providers.HttpProvider('http://kovan.infura.io/'));
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://kovan.infura.io/'));
 
-var ens = new ENS(web3.currentProvider);
+// var ens = new ENS(web3.currentProvider);
 
 
 (() => {
@@ -15,16 +15,16 @@ var ens = new ENS(web3.currentProvider);
 })();
 
 
-// axios.post('http://localhost:9999/api/project/5c82e89ac2af98b4dd1ed031/faq', {
-//     question: 'How much?',
-//     answer: "42",
-//   })
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+axios.post('http://localhost:9999/api/project/5c82d3d168b4e9a45f3743a2/faq', {
+    question: 'How much?',
+    answer: "42",
+  })
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 //   axios.get('http://localhost:9999/api/project/5c82e89ac2af98b4dd1ed031')
 //   .then(function (response) {
