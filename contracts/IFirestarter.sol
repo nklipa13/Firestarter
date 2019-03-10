9c1826575370a380309c7106b3f0036080fbc69c
Nikola Klipa <klipa.nikola@gmail.com>
2019-03-10 05:35:16 +0100

    Minor fixes, add possibility to check vote status

--- contracts/IFirestarter.sol
+++ contracts/IFirestarter.sol
@@ -4,4 +4,5 @@ contract IFirestarter {
     function getNewBalanceAndRateView(uint _id) public view returns(uint, uint);
     function userFundedProject(uint _projectId, address _user) public view returns(uint);
     function withdraw(uint _projectId, uint _ethBalance, uint _daiBalance, string memory _message) public;
+    function maxReputation(uint _projectId) public view returns(uint);
 }
